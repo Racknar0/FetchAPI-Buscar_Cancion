e9ed6ad2edd8ff6ff1394632f20269c13086adb2
Racknar0 <racknarow1@gmail.com>
2022-02-27 21:35:40 -0500

    add validation

---
+++ js/app.js
@@ -0,0 +1,25 @@
+import * as UI from './interfaz.js';
+
+UI.formularioBuscar.addEventListener('submit', buscarCancion);
+
+
+function buscarCancion(e) {
+    e.preventDefault();
+
+    //! obtener Datos del form
+    const artista = document.querySelector('#artista').value;
+    const cancion = document.querySelector('#cancion').value;
+
+    if (artista === '' || cancion === '' ) {
+        // Mostrar error
+        UI.divMensajes.textContent = 'Error... Todos los campos son Obligatorios';
+        UI.divMensajes.classList.add('error');
+
+        setTimeout(() => {
+            UI.divMensajes.textContent = '';
+            UI.divMensajes.classList.remove('error');
+        }, 3000);
+
+        return;
+    }
+}
